71199f2c86ad5f1a09ba76895c43dd2dc73b862b
Paul Barrick <paul@imperativedesign.net>
2012-11-02 11:23:41 -0400

    notes

--- class.init.php
+++ class.init.php
@@ -1,11 +1,26 @@
 <?php
-//To run a phpUNIT test at the command prompt type phpunit --colors <name_of_file>
-//if selenium isnt started it will skip tests
+/**
+ *  Setup Steps:
+ *  ** prereqs:
+ *  - pear channel-discover pear.phpunit.de
+ *  - pear install phpunit/PHPUnit
+ * 
+ *  1) Install selenium RC (remote control) http://seleniumhq.org/projects/remote-control/
+ *  2) Unzip the distribution archive and copy server/selenium-server.jar to /usr/local/bin, for instance.
+ *  3) Start the Selenium RC server by running java -jar /usr/local/bin/selenium-server.jar (make an alias if desired)
+ * 
+ * 
+ *  To run a phpUNIT test at the command prompt type phpunit --colors <name_of_file> 
+ *  Caveats:
+ *  If selenium isnt started it will skip tests 
+ * 
+ */
+
 class SomeTest extends PHPUnit_Extensions_Selenium2TestCase {
 	public function setUp(){
 		 //right now only works w firefox
          $this->setPort(4444);
-		 $this->setBrowser("firefox");
+		 $this->setBrowser('googlechrome');
 	     $this->setBrowserUrl("http://localhost");
 	}	
 	public function testTitle()
